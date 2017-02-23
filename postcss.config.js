6cdf7a0389df8221ca6af8e08b00718943b77bdc
menor <josemenor@gmail.com>
2017-02-23 12:17:26 +0100

    Add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   output: 'build/test.css',
   plugins: [
-    require('postcss-import')({root: 'src'})
+    require('postcss-import')({root: 'src'}),
+    require('autoprefixer')({browsers: '> 5%', })
   ]
 }
