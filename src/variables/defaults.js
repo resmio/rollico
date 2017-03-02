2e27801a0461f40029e19b0daea251e6930f03fc
menor <josemenor@gmail.com>
2017-03-02 07:48:44 +0100

    Fix some errors with forms variables

--- src/variables/defaults.js
+++ src/variables/defaults.js
@@ -1,31 +1,36 @@
 var colors = require('./colors')
 
-var defaults = {
-  // colors
-  actionColor: colors.pacificBlue,
-  actionColorDarker: colors.easternBlue,
-  baseFontColor: colors.emperor,
-  hiperlinkColor: colors.oceanBlue,
-  // borders
-  baseBorderColor: colors.alto,
-  baseBorder: '1px solid ' + colors.alto,
-  baseBorderRadius: '4px',
-  // background colors
-  baseBackgroundColor: colors.white,
-  secondaryBackgroundColor: colors.whiteSand,
-  // typography
-  baseFontSize: '16px',
-  baseLineHeight: '1.5',
-  headingLineHeight: '1.2',
-  baseFontFamily: '"Open Sans", "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif',
-  headingFontFamily: '"Open Sans", "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif',
-  headingFontSize: '24px',
-  // animations
-  baseDuration: '150ms',
-  baseTiming: 'ease',
-  // forms
-  formBoxShadow: 'inset 0 1px 3px ' + colors.gallery,
-  formBoxShadowFocus: 'inset 0 1px 3px ' + colors.gallery + ', 0 0 5px ' + colors.charlotte
-}
+var defaults = (function(){
+    // Stuff we need to reference later
+    var baseBorderColor = colors.alto
+    var formBoxShadow = 'inset 0 1px 3px ' + colors.gallery
+    return {
+      // colors
+      actionColor: colors.pacificBlue,
+      actionColorDarker: colors.easternBlue,
+      baseFontColor: colors.emperor,
+      hiperlinkColor: colors.oceanBlue,
+      // borders
+      baseBorderColor: baseBorderColor,
+      baseBorder: '1px solid ' + baseBorderColor,
+      baseBorderRadius: '4px',
+      // background colors
+      baseBackgroundColor: colors.white,
+      secondaryBackgroundColor: colors.whiteSand,
+      // typography
+      baseFontSize: '16px',
+      baseLineHeight: '1.5',
+      headingLineHeight: '1.2',
+      baseFontFamily: '"Open Sans", "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif',
+      headingFontFamily: '"Open Sans", "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif',
+      headingFontSize: '24px',
+      // animations
+      baseDuration: '150ms',
+      baseTiming: 'ease',
+      // forms
+      formBoxShadow: formBoxShadow,
+      formBoxShadowFocus: formBoxShadow + ', 0 0 5px ' + colors.charlotte
+    }
+})()
 
 module.exports = defaults
