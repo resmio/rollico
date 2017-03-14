da0eca1ad5504de6510d23b3a537c13183af765d
menor <josemenor@gmail.com>
2017-03-14 09:33:38 +0100

    build(library): Exports variable objects as independent objects (colors, sizes, defaults)
    This is an attempt to make imports for variables flexible, we can require all
    
    BREAKING CHANGE: It will probably break all the previous imports, now we need to import by key

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     // ⚠️ order matters!!!
     require('postcss-import')({root: 'src'}),
     require('postcss-nesting')({}),
-    require('postcss-custom-properties')({variables: require('./src/variables')}),
+    require('postcss-custom-properties')({variables: require('./src/variables/index.postcss.js')}),
     require('postcss-calc')({}),
     require('postcss-color-function')({}),
     require('autoprefixer')({browsers: '> 5%'}),
