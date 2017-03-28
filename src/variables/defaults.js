42e4c9d2ff0132947212f661b979e5eac3e9daba
Fabienne Pimenta <fabienne@resmio.com>
2017-03-28 14:10:09 +0200

    feat(styles): changing styles according to resmio styleguide
    Colors, font-sizes, paddings and forms.
    
    BREAKING CHANGE: most of the styles
    
    none

--- src/variables/defaults.js
+++ src/variables/defaults.js
@@ -9,6 +9,7 @@ var defaults = (function(){
       actionColor: colors.pacificBlue,
       actionColorDarker: colors.easternBlue,
       baseFontColor: colors.emperor,
+      headingFontColor: colors.blueBayoux,
       hiperlinkColor: colors.oceanBlue,
       // borders
       baseBorderColor: baseBorderColor,
@@ -20,17 +21,18 @@ var defaults = (function(){
       // typography
       baseFontSize: '16px',
       baseLineHeight: '1.5',
-      headingLineHeight: '1.2',
-      headingFontWeight: '300',
+      headingLineHeight: '2',
+      headingFontWeight: '400',
       baseFontFamily: '"Open Sans", "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif',
       headingFontFamily: '"Open Sans", "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif',
-      headingFontSize: '24px',
+      headingFontSize: '28px',
       // animations
       baseDuration: '150ms',
       baseTiming: 'ease',
       // forms
       formBoxShadow: formBoxShadow,
-      formBoxShadowFocus: formBoxShadow + ', 0 0 5px ' + colors.charlotte
+      formBoxShadowFocus: formBoxShadow + ', 0 0 5px ' + colors.charlotte,
+      textAreaPadding: '5px'
     }
 })()
 
